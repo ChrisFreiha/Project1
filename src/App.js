0ccb0cf880a8c1499a6a06ec7ce3c813d519434f
Chris Freiha <freiha.chris@gmail.com>
2022-08-04 19:36:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from "react";
+import TodoList from "./TodoList";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos"
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  Math.floor(Math.random() * 100); 
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.comlpete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === "") return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: Math.random(), name: name, complete: false}]
+
+    })
+
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Todo List</h2>
+      <input ref={todoNameRef} type="text" /> <br></br>
+
+      <button onClick={handleAddTodo}>Add Item to Todo List</button> 
+      <button onClick={handleClearTodos}>Clear Completed</button>
+
+      <TodoList todos = {todos} toggleTodo = {toggleTodo} class="strikethrough" />
+      
+    </>
   );
+
+
 }
 
 export default App;
